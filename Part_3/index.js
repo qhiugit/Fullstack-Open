8088ef76a65a3492c03647f3e5a2d2fcdfefe537
QuangHieu <quanghieubmtb1@gmail.com>
2025-03-18 17:40:01 +0200

    .

--- Part_3/index.js
+++ Part_3/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 morgan.token("body", (req) =>
   req.method === "POST" ? JSON.stringify(req.body) : " "
 );
@@ -52,7 +54,7 @@ app.post("/api/persons", (req, res) => {
     res.status(200).json({ message: `${name} is added` });
   }
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
